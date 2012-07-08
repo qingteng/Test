e57c4f973a13c3348d49fafbe645bb3aaad2ec60
qingteng <szkyaojiayou@163.com>
2012-07-08 23:01:20 +0800

    sss

---
+++ Test/sss.h
@@ -0,0 +1,13 @@
+//
+//  sss.h
+//  Test
+//
+//  Created by bin huang on 12-7-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sss : UIView
+
+@end
---
+++ Test/sss.m
@@ -0,0 +1,31 @@
+//
+//  sss.m
+//  Test
+//
+//  Created by bin huang on 12-7-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "sss.h"
+
+@implementation sss
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
